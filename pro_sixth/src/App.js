38f9451786323e92d0145f52324bd39cba3a988e
Mansiverma0206 <infomansiverma0206@gmail.com>
2024-03-15 15:36:19 +0530

    'updates'

--- pro_sixth/src/App.js
+++ pro_sixth/src/App.js
@@ -4,22 +4,30 @@ export default class App extends Component {
   constructor() {
     super()
     this.state = {
-      employees: dummy
+      employees: dummy,
+      pageColor : "#fffff"
     }
     this.departments = [
      {key : 1  , value :  "FrontEnd Developer"},
      {key : 2  , value :  "Java Developer"} ,
      {key : 3  , value :  "Android Developer"}
     ]
+    this.bgColors = [
+      {key : 1, value : '#ccaaff'},
+      {key : 2, value : '#B0E0E6'},
+      {key : 3, value : '#00BFFF'},
+      {key : 4, value : '#7B68EE'},
+      {key : 5, value : '#EE82EE'},
+    ]
   }
   
-  // clear = ()=> {
-  //   idbox.value = ""
-  //   namebox.value =""
-  //   salbox.value = ""
-  //   mailbox.value = ""
-  //   this.selectbox.value = ""
-  // }
+  clear = ()=> {
+    this.idbox.value = ""
+    this.namebox.value =""
+    this.salbox.value = ""
+    this.mailbox.value = ""
+    this.selectbox.value = ""
+  }
     
   saveEmp = ()=>{
     var id = this.idbox.value;
@@ -30,11 +38,17 @@ export default class App extends Component {
 
     var ob =  {empid:id,name:name,salary:salary,email:mail,department:dep}
     this.setState({employees:[...this.state.employees,ob]})
-   
+   this.clear()
   }
   render() {
-    return <div className='container'>
+    return <div className='container' style={{backgroundColor:this.state.pageColor}}>
       <h1 className='alert-danger text-center'>Employee Records</h1>
+      <hr/>
+      <div>
+           {this.bgColors.map((ob,index)=><>
+           <button onClick={()=>this.setState({pageColor:ob.value})} style={{backgroundColor:ob.value}}>BG-{index+1}</button>&nbsp;&nbsp;
+           </>)}
+      </div>
       <hr/>
       <div className='container'>
        <h1 className='text-center alert-success'>Add Employees</h1>
@@ -70,22 +84,24 @@ export default class App extends Component {
       <hr/>
       <div className='row'>
         <div className='col-lg-2 col-md-2'>
-          <b className='alert-success'>Total :{this.employees.length}</b>
+          <b className='alert-success'>Total :{this.state.employees.length}</b>
         </div>
         <div className='col-lg-4 col-md-4'>
 
         </div>
         <div className='col-lg-2 col-md-2'>
-          <b className='alert-primary'>FrontEnd Developer : </b>
+          <b className='alert-primary'>FrontEnd Developer :{this.state.employees.reduce((x,ob)=>ob.department=='1'?x+1:x,0)} </b>
         </div>
         <div className='col-lg-2 col-md-2'>
-          <b className='alert-warning'>Java Developer : </b>
+          <b className='alert-warning'>Java Developer :{this.state.employees.reduce((x,ob)=>ob.department=='2'?x+1:x,0)} </b>
         </div>
         <div className='col-lg-2 col-md-2'>
-          <b className='alert-info'>Android Developer : </b>
+          <b className='alert-info'>Android Developer :{this.state.employees.reduce((x,ob)=>ob.department=='3'?x+1:x,0)} </b>
         </div>
       </div>
       <hr />
+      <h1 style={{color:'darkgreen',backgroundColor:'greenyellow',textAlign:'center'}}>Employee Records</h1>
+      <hr/>
       <table className='table table-striped table-hover text-center'>
         <thead>
           <tr>
@@ -98,14 +114,16 @@ export default class App extends Component {
           </tr>
         </thead>
         <tbody>
-          {this.state.employees.map((ob, index) => <tr>
+          {this.state.employees.map((ob, index) =>
+          {
+           return <tr>
             <td>{index + 1}</td>
             <td>{ob.empid}</td>
             <td>{ob.name}</td>
             <td>{ob.salary}</td>
             <td>{ob.email}</td>
-            <td>{ob.department}</td>
-          </tr>)}
+            <td>{this.departments.find(dep=>dep.key==ob.department).value}</td>
+          </tr>})}
         </tbody>
       </table>
     </div>
